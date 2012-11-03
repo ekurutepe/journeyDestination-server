85e182daed438161678215d8906779b7e75c0fb4
Engin Kurutepe <engin@kurutepe.com>
2012-11-03 14:17:32 +0100

    updated url patterns

--- journeyDestinationDjango/urls.py
+++ journeyDestinationDjango/urls.py
@@ -14,4 +14,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+
+
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+    (r'^foursq_auth/', include('Dj4sq.foursq_auth.urls')),
+
 )
