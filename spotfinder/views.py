825bbe6c240e6d098fa9d272bafcc7d862a2d892
Moritz Schäfer <mollitz@mollitz-lifebook.(none)>
2012-11-03 17:05:46 +0100

    Ready for using

--- spotfinder/views.py
+++ spotfinder/views.py
@@ -1,26 +1,55 @@
 from django.http import HttpResponse
-import journeyDestinationDjango.settings
+from journeyDestinationDjango.settings import FOURSQ_CLIENT_ID, FOURSQ_CLIENT_SECRET
+#from django.core import serializers
+from django.utils import simplejson
+import foursquare
+
 # Create your views here.
 def index(request):
     """
     Show up the Google maps form here
     """
+    output = []
     try:
         points = request.POST['points']
     except:
-        return HttpResponse("Dont call me without points")
+        #return HttpResponse("Dont call me without points")
+        output.append("Be careful. We are working with test values!<br>\n")
+        points = '["52.706347,10.442505","52.513714,13.353676"]'
     try:
         client = foursquare.Foursquare(access_token=request.POST['apiToken'])
     except:
-        client = foursquare.Foursquare(client_id='YOUR_CLIENT_ID', client_secret='YOUR_CLIENT_SECRET')
+        client = foursquare.Foursquare(client_id=FOURSQ_CLIENT_ID, client_secret=FOURSQ_CLIENT_SECRET)
 
     #Parse the points
+    points = simplejson.loads(points)
 
+    #TWEAK THIS later!
+    #more infos here: https://developer.foursquare.com/docs/venues/explore
+    defaultParams = {'radius': 10000.0, 'section': 'topPicks', 'limit': 1000, 'venuePhotos': 0}
+    import pdb
+    pdb.set_trace()
+    venues = []
     for point in points:
-        client.venues.explore("{},{}".format
+        defaultParams['ll'] = point
+        venuesJson = client.venues.explore(params=defaultParams)
+        venue = getMostInterestingPoint(venuesJson, 2)
+        if venue:
+            venues.append(str(venue))
 
-    return HttpResponse("Insert Google Maps Source and Destination here")
+    return HttpResponse("<br><br><br>".join(venues))
 
 
 #api call:
 #https://api.foursquare.com/v2/venues/explore
+
+
+def getMostInterestingPoint(venuesJson, minVal):
+    """Return the dictionary of the highest rated point if it exceeds minVal"""
+    highestVenue = None
+    for venue in venuesJson[u'groups'][0]['items']:
+        checkIns = venue[u'venue'][u'stats'][u'checkinsCount']
+        if checkIns > minVal:
+            if not highestVenue or highestVenue[u'venue'][u'stats'][u'checkinsCount'] < checkIns:
+                highestVenue = venue
+    return highestVenue
